21bbc19d716840aa01a6754510088a57326f5a85
Erick Hiroshi <erickhiroshi4@gmail.com>
2023-10-30 20:56:15 -0300

    v3

--- Scripts/Aleatoriedade/Logica.js
+++ Scripts/Aleatoriedade/Logica.js
@@ -1,41 +1,3 @@
-function Calcular() {
-    const Valor1 = parseFloat(document.getElementById("num1").value);
-    const Valor2 = parseFloat(document.getElementById("num2").value);
-
-    let Resultado;
-
-    var Verificação = function () {
-        if (Valor1 > Valor2) {
-            return "1 Maior que o 2"
-        }
-        else if (Valor1 < Valor2) {
-            return "2 Maior que o 1"
-        }
-        else if (Valor1 == Valor2) {
-            return "2 Valores Iguais"
-        }
-    }
-    Resultado = document.getElementById("Resultado").textContent = Verificação()
-}
-
-
-function CheckBox() {
-    const Check1 = parseFloat(document.getElementById("Check1").checked);
-    const Check2 = parseFloat(document.getElementById("Check2").checked);
-
-    let Resultado;
-
-    var VerificaçãoCheck = function () {
-        if (Check1 && Check2) {
-            return "True"
-        }
-        else
-            return "False"
-    }
-    Resultado = document.getElementById("ResultadoCheck").textContent = VerificaçãoCheck()
-}
-
-
 const RaridadeRoll = { "Normal": 150, "Raro": 50, "Epico": 10, "Lendario": 1 };
 
 
@@ -52,6 +14,7 @@ const Frutas = {
     "Gomu": RaridadeRoll.Lendario,
     "Magnet": RaridadeRoll.Lendario,
 }
+
 function RandomFruit() {
     const Tabela = [];
 
